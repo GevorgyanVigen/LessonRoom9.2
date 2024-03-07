6bcbed532dde072db40832bdb6cb45acbc8a9b18
GevorgyanVigen <vigengevorgyan45@gmail.com>
2024-03-07 21:59:27 +0400

    first commit

---
+++ src/lesson/room/Test.java
@@ -0,0 +1,12 @@
+package lesson.room;
+
+// debug
+public class Test {
+    public static void main(String[] args) {
+
+        int x = 14, y = 6;
+
+        boolean b = x > 1 || x < 10;
+        System.out.println(b);
+    }
+}
